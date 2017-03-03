a58f41ce79e8ddc8b8d4074ffd137e4f2efa7996
cybervaca <cybervaca@gmail.com>
2017-03-03 11:39:16 +0100

    Add files via upload

--- PSBoTelegram.ps1
+++ PSBoTelegram.ps1
@@ -1,6 +1,6 @@
 clear
 $ErrorActionPreference = "SilentlyContinue"
-$version = "0.7"
+$version = "0.8"
 
 $banner = "    ____  _____ ____      ______     __                              
    / __ \/ ___// __ )____/_  __/__  / /__   ____ __________ _____ __
